cc98c700ac9f1aaddc569e2efcb9b91eebfd0ccb
asrafulsheikhasif143 <168800910+asrafulsheikhasif143@users.noreply.github.com>
2025-04-08 01:01:30 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMessage() {
+  document.getElementById("message").textContent = "Hello ! Keep working on your dreams!";
+}
+
+function openFacebook() {
+  window.open("https://www.facebook.com/share/15JkAdkjMh/", "_blank");
+}
+
+function openWhatsApp() {
+  window.open("https://wa.me/8801876149129", "_blank");
+}
